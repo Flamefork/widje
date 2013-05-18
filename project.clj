562daae57a4da27cb6ebb6786fae42882757f313
Kevin Albrecht <onlyafly@gmail.com>
2013-05-18 10:31:12 +0200

    Update version and fork project name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject widje "0.1.7-SNAPSHOT"
+(defproject onlyafly/widje "0.1.7"
   :description "Templating System for ClojureScript"
   :url "https://github.com/Flamefork/widje"
   :license {:name "Eclipse Public License - v 1.0"
